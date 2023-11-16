dc8b90f7e6d70a40c85dfe923d61f0dd091b0eaf
Yrol <yrollgayanth@yahoo.com>
2023-11-16 20:57:02 +1100

    Adding basic Flux and Mono, and adding test cases

--- src/main/java/blog/yrol/ReactiveMonoFlux.java
+++ src/main/java/blog/yrol/ReactiveMonoFlux.java
@@ -5,11 +5,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @SpringBootApplication
 public class ReactiveMonoFlux implements CommandLineRunner {
     
     public static final Logger LOG = LoggerFactory.getLogger(ReactiveMonoFlux.class);
+
+
+    /**
+     * Returning a dataset / stream of data using Flux. In reality this will be coming from a DB or an external service
+     * Using optional log() for printing the events
+     * **/
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe"))
+                .log();
+    }
+
+    /**
+     * Returning a single element data
+     * Using optional log() for printing the events
+     * **/
+    public Mono<String> nameMono() {
+        return Mono.just("James").log();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveMonoFlux.class, args);
@@ -18,5 +41,20 @@ public class ReactiveMonoFlux implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         LOG.info("ReactiveMonoFlux started....");
+
+        /**
+         * Accessing the flux values
+         * To access we need to subscribe to it first using the function - subscribe().
+         * All the values in Flux will be return in a stream one by one
+         * Using lambdas to loop through Flux and Mono data to print data
+         * **/
+        ReactiveMonoFlux reactiveMonoFlux = new ReactiveMonoFlux();
+        reactiveMonoFlux.namesFlux().subscribe(name -> {
+            System.out.println("(Flux) Name is : " + name);
+        });
+
+        reactiveMonoFlux.nameMono().subscribe(name -> {
+            System.out.println("(Mono) Name is : " + name);
+        });
     }
 }
--- src/test/java/blog/yrol/ReactiveMonoFluxTest.java
+++ src/test/java/blog/yrol/ReactiveMonoFluxTest.java
@@ -1,4 +1,57 @@
 package blog.yrol;
 
+
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+/**
+ * Test case creation for Reative Flux
+ * Reative Flux cannot be tested with Junit Assertions, hence a special library is being used - reactor-test
+ * **/
 public class ReactiveMonoFluxTest {
+
+    ReactiveMonoFlux reactiveMonoFlux = new ReactiveMonoFlux();
+
+
+    /**
+     * Testing the namesFlux
+     * **/
+    @Test
+    void testNames_whenCallingNamesFlux_returnNames(){
+
+        // Arrange
+        var namesFlux = reactiveMonoFlux.namesFlux();
+
+        // Act and Assert
+        StepVerifier.create(namesFlux)
+                .expectNext("Alex", "Ben", "Chloe")
+                .verifyComplete();
+    }
+
+    @Test
+    void testNameCount_whenCallingNamesFlux_returnElementCount(){
+
+        // Arrange
+        var namesFlux = reactiveMonoFlux.namesFlux();
+
+        // Act and Assert
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    /**
+     * Testing the remaining number of elements after verifying the first element
+     * **/
+    @Test
+    void testNameCountMixAndMatch_whenCallingNamesFlux_returnElementAndRemainingCount(){
+        // Arrange
+        var namesFlux = reactiveMonoFlux.namesFlux();
+
+        // Act and Assert
+        StepVerifier.create(namesFlux)
+                .expectNext("Alex")
+                .expectNextCount(2)
+                .verifyComplete();
+    }
 }
